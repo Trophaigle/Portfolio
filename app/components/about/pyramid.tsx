8c6653174080cc034ee7a6a71b4fe729efe35654
serafith <serafith@serafith-Nitro-AN515-54>
2026-03-27 11:33:43 +0100

    rm pyramid

--- app/components/about/pyramid.tsx
+++ app/components/about/pyramid.tsx
@@ -8,15 +8,6 @@ const Pyramid = () => {
 
   return (
     <div className="bg-black flex flex-col items-center justify-center py-12 px-10">
-      <h1 className="text-white text-3xl mb-6">Pyramide</h1>
-      <div className="relative h-[600px] w-full max-w-md">
-        <Image
-          src="/images/self-care-rogne-black.png"
-          alt="My image"
-          fill
-          className="object-contain"
-        />
-      </div>
       <div className="mt-12 text-center px-6 md:px-25">
         <p className="text-white text-xl md:text-2xl">
           {t("quote-pyramid")}
