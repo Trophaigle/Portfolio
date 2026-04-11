4d9cfa9db6628eb091abc00f724ff3b0b8048373
Trophaigle <thom1.serafin@gmail.com>
2026-04-11 18:11:48 +0200

    add visual audio exp

--- app/[locale]/(root)/art/page.tsx
+++ app/[locale]/(root)/art/page.tsx
@@ -79,7 +79,7 @@ export default function ArtHero() {
         />
 
         <button
-          onClick={() => triggerZoomAndNavigate("/art/threejs")}
+          onClick={() => triggerZoomAndNavigate("https://visual-audio-experience-threejs.vercel.app/")}
           className="
             absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
             text-white font-semibold text-2xl
